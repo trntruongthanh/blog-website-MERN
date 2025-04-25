e1163948334a562ef0be4a83e835c4da136e3369
trntruongthanh <trntruongthanh@gmail.com>
2025-04-25 09:51:24 +0700

    Working Search Input

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 import { UserContext } from "../App";
 
@@ -11,7 +11,12 @@ import UserNavigationPanel from "./user-navigation.component";
 
 const Navbar = () => {
   const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+
   const [userNavPanel, setUserNavPanel] = useState(false);
+
+  let navigate = useNavigate()
+
+  //===========================================================================================
 
   const {
     userAuth,
@@ -27,12 +32,23 @@ const Navbar = () => {
     setUserNavPanel((current) => !current);
   };
 
-  // setTimeout giúp đảm bảo người dùng có thể click vào mục trong menu trước khi nó bị ẩn đi. 
+  // setTimeout giúp đảm bảo người dùng có thể click vào mục trong menu trước khi nó bị ẩn đi.
   const handleBlur = () => {
     setTimeout(() => {
       setUserNavPanel(false);
     }, 200);
   };
+
+  //===========================================================================================
+
+  const handleSearch = (event) => {
+
+    let query = event.target.value;
+
+    if (event.key === "Enter" && query) {
+      navigate(`/search/${query}`)
+    }
+  }
 
   return (
     <>
@@ -42,19 +58,18 @@ const Navbar = () => {
           <img src={images.logo} className="w-full" />
         </Link>
 
-        <p className="p-4 m-[-20px] text-nowrap">Touch Decor</p>
-
         {/*INPUT */}
         <div
           className={
-            "absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:bottom-0 md:block md:relative md:inset-0 md:p-0 md:w-auto " +
-            (searchBoxVisibility ? "show" : "hide")
+            (searchBoxVisibility ? "block" : "hidden") +
+            "md:block absolute md:relative bg-white w-full md:w-auto left-0 top-full md:top-auto mt-0.5 md:mt-0 border-b border-grey py-4 px-[5vw] md:px-0"
           }
         >
           <input
+            onKeyDown={handleSearch}
             type="text"
             placeholder="Search"
-            className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+            className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 "
           />
           <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
         </div>
