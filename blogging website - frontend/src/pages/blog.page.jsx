d56b4d935b6720853bc056a09982fa33f95950a3
trntruongthanh <trntruongthanh@gmail.com>
2025-05-16 11:39:26 +0700

    Fetching Blog Details

--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,11 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const BlogPage = () => {
-    const  { blog_id } = useParams()
+export const blogStructure = {
+  title: "",
+  des: "",
+  content: [],
+  tags: [],
+  author: { personal_info: {} },
+  banner: "",
+  publishedAt: "",
+};
 
-    return (
-        <h1>This is a blogs page - {blog_id}</h1>
-    );
-}
+const BlogPage = () => {
+  const { blog_id } = useParams();
+
+  const [blog, setBlog] = useState(blogStructure);
+
+  const {
+    title,
+    content,
+    banner,
+    author: {
+      personal_info: { fullname, username, profile_img },
+    },
+    publishedAt,
+  } = blog;
+
+  // ========================================================================================
+
+  const fetchBlog = async () => {
+    try {
+      const {
+        data: { blog },
+      } = await axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", {
+        blog_id,
+      });
+
+      if (!blog) {
+        return;
+      }
+
+      setBlog(blog);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
+
+  // =======================================================================================
+
+  return (
+    <div>
+      <h1>This is a blog page - {title}</h1>
+    </div>
+  );
+};
 
 export default BlogPage;
